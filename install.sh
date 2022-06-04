ba414b3230371be6c880a92505d41e0d3e383eec
Jiaxuan Shang <jiaxuansh@gmail.com>
2022-06-03 17:38:15 -0700

    Create install.sh

--- install.sh
+++ install.sh
@@ -12,25 +12,15 @@ sudo apt-get install -y ack
 echo "\n\n===== Install FZF ====="
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
-echo "\n\n===== Install NeoVim ====="
-curl -fLo  "${HOME}/tmp/nvim/nvim.appimage" --create-dirs https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x "${HOME}/tmp/nvim/nvim.appimage"
-${HOME}/tmp/nvim/nvim.appimage --appimage-extract
-sudo mv ./squashfs-root /opt/nvim
-sudo ln -sf /opt/nvim/AppRun /usr/bin/nvim
-
-echo "\n\n===== Remove RVM ====="
-# From https://stackoverflow.com/a/25571648/33226
-sudo /usr/local/rvm/bin/rvm implode --force 2>&1 >/dev/null
-rm -rf /usr/local/rvm
-sudo rm -f /etc/profile.d/rvm.sh
-
-echo "\n\n===== Install vim-plug ====="
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 echo "\n\n===== Installing oh-my-zsh ====="
 sudo rm -fr ${HOME}/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cat .zshrc > $HOME/.zshrc
+cat .p10k.zsh > $HOME/.p10k.zsh
 
 echo "\n\n===== Clean up things we don't want ====="
 sudo rm -f /usr/bin/gs
@@ -40,8 +30,4 @@ sudo ln -s /usr/bin/nvim /usr/bin/vi
 echo "\n\n===== Symlink dotfiles ====="
 mkdir -p "${HOME}/.config"
 ln -sf ${DOTFILES_DIR}/zshrc ${HOME}/.zshrc
-ln -sf ${DOTFILES_DIR}/gitconfig ${HOME}/.gitconfig
-ln -sf ${DOTFILES_DIR}/config/nvim ${HOME}/.config/nvim
 
-echo "\n\n===== Install VIM plugins ====="
-vi --headless +PlugInstall +qall
